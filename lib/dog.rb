0de961807028f2dbb91c8b4f584d5722a42370a4
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-09-03 04:56:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name = (dog_name)
+    @this_dogs_name
+  end
+end
+  
+  def bark
+    puts "Woof!"
+  end
+
+  
+fido = Dog.new 
+fido.name = "Fido"
+fido.name
+
+fido.bark 
+
